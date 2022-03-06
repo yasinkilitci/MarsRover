87ed85813e9e5835eea42ec30a624689ddc4a4d9
yasinkilitci <yasinkilitci@gmail.com>
2022-03-07 01:01:25 +0300

    Unit Test Commit

---
+++ MarsRoverTest/MarsRoverTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MarsRover;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MarsRoverTest
+{
+    [TestClass]
+    public class MarsRoverTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Plateau plateauOne = new Plateau(new Position(5, 5));
+            Rover firstRover = new Rover(plateauOne, new Position(1, 2), Directions.N);
+            firstRover.Process("LMLMLMLMM");
+
+
+            Plateau plateauTwo = new Plateau(new Position(5, 5));
+            Rover secondRover = new Rover(plateauTwo, new Position(3, 3), Directions.E);
+            secondRover.Process("MMRMMRMRRM");
+
+            Console.WriteLine(firstRover.ToString());
+            Console.WriteLine(secondRover.ToString());
+        }
+    }
+}
---
+++ MarsRoverTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MarsRoverTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MarsRoverTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6170eb7d-493e-41e3-afe9-487112472f7a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
